8f7261d2c46e18a6de28173b003f7ef448dbde94
FelipePinzon <fepinzon77@gmail.com>
2023-04-11 23:19:08 -0500

    bottombar

--- app/src/main/java/com/example/modelo1giigs/MainActivity.java
+++ app/src/main/java/com/example/modelo1giigs/MainActivity.java
@@ -1,19 +1,51 @@
 package com.example.modelo1giigs;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.widget.Toast;
 
 import com.an.biometric.BiometricCallback;
 import com.an.biometric.BiometricManager;
+import com.example.modelo1giigs.databinding.ActivityMainBinding;
+
 
 public class MainActivity extends AppCompatActivity implements BiometricCallback {
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_main);
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNaigationView.setOnItemSelectedListener(item -> {
+
+            switch (item.getItemId()){
+
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.user:
+                    replaceFragment(new UserFragment());
+                    break;
+                case R.id.config:
+                    replaceFragment(new ConfigFragment());
+                    break;
+                case R.id.añadir:
+                    replaceFragment(new AddFragment());
+                    break;
+                case R.id.archivo:
+                    replaceFragment(new ArchivoFragment());
+                    break;
+            }
+            return true;
+        });
 
         new BiometricManager.BiometricBuilder(MainActivity.this)
                 .setTitle("Login Principal")
@@ -22,6 +54,13 @@ public class MainActivity extends AppCompatActivity implements BiometricCallback
                 .setNegativeButtonText("Cancelar")
                 .build()
                 .authenticate(MainActivity.this);
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout,fragment);
+        fragmentTransaction.commit();
     }
 
     @Override
